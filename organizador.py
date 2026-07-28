ba8b4e2d0142dc1c79b3bd6d80b32e73babebe2f
V7CYBER <v7cybersec@gmail.com>
2026-07-28 14:11:17 +0200

    v1.8 - Confirmación previa a la organización

--- organizador.py
+++ organizador.py
@@ -83,7 +83,7 @@ def main():
     while True:
 
         print("=" * 40)
-        print("        FILE ORGANIZER v1.7")
+        print("        FILE ORGANIZER v1.8")
         print("=" * 40)
         print("1) Organizar carpeta")
         print("2) Deshacer última organización")
